de3144745e6c6fe936644a927c526c8edcde5446
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2021-04-26 21:58:23 +0530

    new Changes

---
+++ Ver_3.0/Main1.java
@@ -0,0 +1,6 @@
+public class Main1{
+    public static void main(String args[]){
+        String myStr=new String("Cheggers2012");
+        System.out.print(myStr.substring(2,myStr.substring(0,2).size()));
+    }
+}
