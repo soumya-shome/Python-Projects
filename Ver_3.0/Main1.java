9916510d13a8c5821ba5c2718f6dad542b8c1917
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2021-05-23 14:25:33 +0530

    final

--- Ver_3.0/Main1.java
+++
@@ -1,6 +0,0 @@
-public class Main1{
-    public static void main(String args[]){
-        String myStr=new String("Cheggers2012");
-        System.out.print(myStr.substring(2,myStr.substring(0,2).size()));
-    }
-}
