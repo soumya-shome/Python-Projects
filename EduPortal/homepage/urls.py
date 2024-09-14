eb1b1186107784145b07a06a26d599ed0b591da7
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2024-09-14 15:03:58 +0000

    updated

--- EduPortal/homepage/urls.py
+++ EduPortal/homepage/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('api/', views.my_api, name='my_api'),  # API endpoint
+    path('api-data/', views.api_data, name='api_data'),  # View to call the API
 ]
--- EduPortal/homepage/views.py
+++ EduPortal/homepage/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
 
 def index(request):
     return render(request, 'homepage/index.html')
+
+def register(request):
+    return render(request, 'homepage/register.html')
+
+def my_api(request):
+    data = {
+        'message': 'Hello, this is your API response!',
+        'items': ['Item 1', 'Item 2', 'Item 3']
+    }
+    return JsonResponse(data)
+
+def api_data(request):
+    return render(request, 'homepage/api_data.html')
+    # response = requests.get('http://127.0.0.1:8000/api/')  # URL of the API endpoint
+    # data = response.json()
+    # return render(request, 'homepage/api_data.html', {'data': data})
